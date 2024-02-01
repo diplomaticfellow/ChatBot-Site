f363df40d4a0774d205168c6c26d65c7d90e69e5
danial1jeelani <158510199+danial1jeelani@users.noreply.github.com>
2024-02-02 00:24:54 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function redirectToChatbot() {
+    // Replace this URL with the actual chatbot URL
+    window.location.href = 'https://mediafiles.botpress.cloud/046a9bfb-980a-43b2-a59e-772b7826f49c/webchat/bot.html';
+}
